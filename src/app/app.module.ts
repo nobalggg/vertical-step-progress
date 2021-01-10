dad8c859de124cb532faee22d0d8b0676cae8291
nobalggg <43821759+nobalggg@users.noreply.github.com>
2021-01-11 01:18:23 +0530

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,15 +6,13 @@ import { AppComponent } from "./app.component";
 import { MatCardModule } from "@angular/material/card";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { HttpClientModule } from "@angular/common/http";
-import { MainComponent } from "./main/main.component";
-import { TableComponent } from "./table/table.component";
-import { TDformComponent } from "./tdform/tdform.component";
+
 import { FormsModule } from "@angular/forms";
 import { from } from "rxjs";
 import { StepProgressComponent } from './step-progress/step-progress.component';
 
 @NgModule({
-  declarations: [AppComponent, MainComponent, TableComponent, TDformComponent, StepProgressComponent],
+  declarations: [AppComponent,  StepProgressComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
